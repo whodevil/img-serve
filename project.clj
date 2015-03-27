6ae0530b9ed99fabe41e6395ca4cf42f4cf45894
whodevil <whodevil@offthecob.info>
2015-03-26 22:00:50 -0700

    updating libraries and adding stuart sierra's component

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject img-serve "0.1"
   :description "Micro-service to serve up images"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.5"]
-                 [compojure "1.1.8"]
-                 [com.taoensso/timbre "3.3.1"]
-                 [http-kit "2.1.16"]]
+                 [org.clojure/data.json "0.2.6"]
+                 [compojure "1.3.2"]
+                 [com.taoensso/timbre "3.4.0"]
+                 [http-kit "2.1.19"]
+                 [com.stuartsierra/component "0.2.3"]]
 
   :main ^:skip-aot img-serve.main)
